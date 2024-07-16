9e5e8368c29ae09a1cf67df455e71a44174c1cc5
Jordan Gardner <jordanthomasg@gmail.com>
2024-07-16 15:03:16 -0600

    Add tailwind

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -23,12 +23,16 @@ export default function Popup() {
   };
 
   return (
-    <main>
-      <div className="title-container">
-        <img src="default_icon.png" alt="AdBlocker Blocker Buster icon" />
-        <h1>AdBlocker Blocker Buster</h1>
-      </div>
-      <div className="actions-container">
+    <main className="p-2 min-w-72">
+      <div className="flex flex-col gap-y-4 actions-container">
+        <div className="flex justify-between items-center w-full">
+          <img
+            src="default_icon.png"
+            alt="AdBlocker Blocker Buster icon"
+            className="w-12"
+          />
+          <h1 className="text-base font-bold">AdBlocker Blocker Buster</h1>
+        </div>
         <p>
           Pop-ups and modals should be removed auto-magically. If you are still
           seeing pop-ups or modals, try removing them manually or report an
@@ -39,6 +43,7 @@ export default function Popup() {
             id="manual"
             onClick={() => handleAction("manual")}
             disabled={loading === "manual"}
+            className="border border-gray-400 h-10 w-full mb-2"
           >
             {loading === "manual" ? "Removing..." : "Remove blockers manually"}
           </button>
@@ -46,6 +51,7 @@ export default function Popup() {
             id="report"
             onClick={() => handleAction("report")}
             disabled={loading === "report"}
+            className="border border-gray-400 h-10 w-full"
           >
             {loading === "report" ? "Reporting..." : "Report an issue"}
           </button>
